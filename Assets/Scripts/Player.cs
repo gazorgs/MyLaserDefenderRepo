a89bcf2bee0fc54b791fdc5dda04655f0c4bb3a2
gazorgs <gazorgs@hotmail.com>
2020-08-21 20:49:49 +0100

    Laser Defender - post lecture 89
    Player.cs - ViewportToWorldPoint() and Mathf.Clamp() to set Player move boundaries, with some padding added as a SerializedField

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,11 +7,27 @@ public class Player : MonoBehaviour
 {
     // Configuration parameters
     [SerializeField] float moveSpeed = 10f; // change this in the Inspector to get the Player move speed to what feels best
-    
+    [SerializeField] float padding = 0.5f;
+    //[SerializeField] float yPadding = 1f;  //maybe implement later to stop Player moving too far up the screen
+
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetUpMoveBoundaries();
+    }
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main; // need to say what camera we are referring to, so declare it as a local variable here
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding; // what is the World space value for the x element of our ViewportToWorldPoint
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding; // far right of screen
+
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding; // what is the World space value for the y element of our ViewportToWorldPoint
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding; // top of screen
     }
 
     // Update is called once per frame
@@ -22,14 +38,12 @@ public class Player : MonoBehaviour
 
     private void Move()
     {
-        //in Unity go to Edit - Project Settings - Input Manager (chose Horizontal and Vertical keys and mouse axis for this one)
+        //in Unity go to Edit - Project Settings - Input Manager (chose Horizontal and Vertical keys and mouse/gamepad axis for this one)
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed; //Time.delta.Time ensures framerate independent whether on fast or slow computer
-        //Debug.Log(deltaX);
-        var newXPos = transform.position.x + deltaX; //new Player position equal to current position plus the key press above
-
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        //Debug.Log(deltaY);
-        var newYPos = transform.position.y + deltaY;
+        
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax); //new Player x position equal to current position plus the key press above, clamped to fit
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax); 
 
         transform.position = new Vector2(newXPos, newYPos); //Move Player to new position
     }
