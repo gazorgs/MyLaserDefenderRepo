a47b81351168520139704c91faccfa6a7720e5da
gazorgs <gazorgs@hotmail.com>
2020-08-24 17:38:57 +0100

    Laser Defender - post lecture 95
    Create list of enemy waypoints - created EnemyPathing.cs

---
+++ Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    // Configuration parameters
+    [SerializeField] List<Transform> waypoints;
+}
