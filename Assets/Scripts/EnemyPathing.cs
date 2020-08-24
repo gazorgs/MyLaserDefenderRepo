a664274adb35b61601a4e5e1c7e005652bd7721f
gazorgs <gazorgs@hotmail.com>
2020-08-24 21:45:32 +0100

    Laser Defender - post lecture 96
    Use Vector2.MoveTowards() method to move enemy ship along waypoints

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -6,4 +6,37 @@ public class EnemyPathing : MonoBehaviour
 {
     // Configuration parameters
     [SerializeField] List<Transform> waypoints;
+    [SerializeField] float moveSpeed = 2f;
+    int waypointIndex = 0;
+
+    void Start()
+    {
+        transform.position = waypoints[waypointIndex].transform.position; // position of enemy is the current waypointIndex in the waypoints list - i.e. go to zero
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            var targetPosition = waypoints[waypointIndex].transform.position; // where we want to move to
+            var movementThisFrame = moveSpeed * Time.deltaTime; // how fast we want to move
+            transform.position = Vector2.MoveTowards //Vector2.MoveTowards takes 3 parameters, current position, target position, speed of movement
+                (transform.position, targetPosition, movementThisFrame);
+
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 }
+
