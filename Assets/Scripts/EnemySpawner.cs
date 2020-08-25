bf95511c060380e5887893f0e0c0b3a084fed580
gazorgs <gazorgs@hotmail.com>
2020-08-25 16:47:18 +0100

    Laser Defender - post lecture 99
    Spawn multiple enemies per wave - created EnemySpawner.cs with coroutine to instantiate enemies by calling various values within WaveConfig.cs

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] List<WaveConfig> waveConfigs;
+    int startingWave = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        var currentWave = waveConfigs[startingWave];
+        StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+    }
+
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig)
+    {
+        for (int enemyCount = 0; enemyCount < waveConfig.GetNumberOfEnemies(); enemyCount++)
+        {
+            Instantiate(                                                    // Instantiate()
+            waveConfig.GetEnemyPrefab(),                                // the 'what'
+            waveConfig.GetWaypoints()[0].transform.position,            // the 'where'
+            Quaternion.identity);                                       // the 'rotation'
+            yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
+        }
+    }
+}
