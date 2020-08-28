b5a9829eb98959cc144332a50217eda0cddc2179
gazorgs <gazorgs@hotmail.com>
2020-08-28 17:04:00 +0100

    Laser Defender - post lecture 105
    Make Enemy Shoot - added new enemy laser sprite, new enemy laser prefab, added Fire() method to Enemy.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,21 +1,47 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] float health = 100;
-    
+    [SerializeField] float shotCounter;
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+    [SerializeField] GameObject enemyLaserPrefab = default;
+    [SerializeField] float enemyProjectileSpeed = 10f;
+    [SerializeField] float padding = 0.5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        shotCounter = UnityEngine.Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CountDownAndShoot();
+    }
+
+    private void CountDownAndShoot()
+    {
+        shotCounter -= Time.deltaTime; //each frame count down by how long a frame takes to execute
+        if (shotCounter <= 0f)
+        {
+            Fire();
+            shotCounter = UnityEngine.Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject enemyLaser = Instantiate(
+                    enemyLaserPrefab,
+                    transform.position + new Vector3(0, -padding, 0), //offset the laser from the centre pivot of the enemy ship slightly
+                    Quaternion.identity) as GameObject;
+        enemyLaser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -enemyProjectileSpeed);
     }
 
     private void OnTriggerEnter2D(Collider2D other) // you can change the default 'collision' to anything, in this case 'other' to denote the 'other' thing bumping into Enemy
