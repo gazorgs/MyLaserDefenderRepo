1e85505a8705ffc3cacf6575a88db7457db1be67
gazorgs <gazorgs@hotmail.com>
2020-09-03 19:17:17 +0100

    Laser Defender - post lecture 114
    Added MusicPlayer.cs with singleton to play same track through each of the three scenes.  Also added 2 extra tracks in case a different track is required for each scene later.

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    void Awake()
+    {
+        SetUpSingleton();
+    }
+
+    private void SetUpSingleton()
+    {
+        if (FindObjectsOfType(GetType()).Length > 1) // GetType will return the type of class, in this case MusicPlayer.cs - if there is more than 1 destroy the new one trying to be called
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
